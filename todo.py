9c71aeafaaa2e4f1b1658e0b657a4e860571a01e
Rachel Kelly <scrgkelly@gmail.com>
2018-04-26 15:39:05 -0500

    Updated to do list. Removed bugs that have been fixed. Updated the hyphen statuses.

--- todo.py
+++ todo.py
@@ -1,14 +1,13 @@
 """
 TODO:
-#- Intelligence to project this week's grocery bill (still need some kind of inventory tracker and place to add misc foods like snacks/bfast)
+-- Intelligence to project this week's grocery bill (still need some kind of inventory tracker and place to add misc foods like snacks/bfast)
     - Define own conversions for foods that have mass for purchase_history and volume for recipes (i.e. pb, flour, salsa, bbq sauce, etc.)
 -- Add 'add to inventory' check box for enter prices screen, then don't have to enter
   price but just amount and ingredient so that the pantry inventory is accurate
   and another use case is that then items that I want to enter prices for but
   didn't actually buy won't affect the inventory
 -- When choose main dish, auto select sides based on most common pairing
-~~~~~ Make amount label on configure ingredients window to be unique -- currently throwing error if access that window for more than one recipe.
-~ Verify that calculations are accurate for report (esp. min/max values)
+- Verify that calculations are accurate for report (esp. min/max values)
 - Store extra reminder (True/False and length) info in db instead of hard code in calendar.py
 - Copy from week feature for meal plan. Can copy meal assignments from a previous week to current one...
 - Generate shopping list button on meal plan tab. That would display all ingredients needed (and prices?). Could edit prices/amounts and then generate budget prediction.
@@ -18,7 +17,6 @@ TODO:
 - Tab to display inventory for ingredients (basically ingredient info page -- price per unit / last 5 purchase history, inventory, recipes...)
 - Be able to update the recipe's name
 - Streamline casing for ingredients added to db and update current ones to fit. First letter capped and all the rest lower.
-~ Errors with recipe add query are not creating ui pop-up
 - On date go, add column for calendar events that would affect meals
 - Lunch and breakfast and snacks capabilities
 - Add recipes to 'cookbook' in slides (attribute in recipe table for distinguishing dessert from main from soup...)
